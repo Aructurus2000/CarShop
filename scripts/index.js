9388a623c2c2bcd9cfd0bb0d0ca3be4a3258c622
arthur_burzhumov <arturb2000@mail.ru>
2025-02-21 00:19:36 +0300

    feat: добавил обработчики событий для фильтров

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const filterItems = document.querySelectorAll('.cars-filter li');
+const carItems = document.querySelectorAll('.car');
+const carsContent = document.getElementById('cars-content');
+
+filterItems.forEach((item) => {
+    item.onclick = () => {
+        filterItems.forEach((el) => el.classList.remove('active'));
+        item.classList.add('active');
+
+        const filterText = item.textContent.toLowerCase();
+
+        carItems.forEach((car) => {
+            if (
+                filterText === 'все марки' ||
+                car.querySelector('h4').textContent.toLowerCase().includes(filterText)
+            ) {
+                car.style.display = 'flex';
+            } else {
+                car.style.display = 'none';
+            }
+        });
+
+        carsContent.scrollIntoView({ behavior: 'instant' });
+    };
+});
